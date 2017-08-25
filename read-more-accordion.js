abdddc5927949b3b08f40a8da5661a8f02ce7f28
Dimitris Mavroudis <im.dimitrismavroudis@gmail.com>
2017-08-25 17:34:28 +0300

    Javascript error on parsing elements

--- read-more-accordion.js
+++ read-more-accordion.js
@@ -1,19 +1,19 @@
 var acc = document.getElementsByClassName("accordion");
 
-acc.forEach(function(e) {
-    e.onclick = function() {
+for (let i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
         this.classList.toggle("active");
         this.nextElementSibling.classList.toggle("tease");
         this.nextElementSibling.classList.toggle("show");
     }
-    e.onmouseover = function() {
+    acc[i].onmouseover = function() {
         if (!this.classList.contains("active")) {
             this.nextElementSibling.classList.add("tease");
         }
     }
-    e.onmouseout = function() {
+    acc[i].onmouseout = function() {
         if (!this.classList.contains("active")) {
             this.nextElementSibling.classList.remove("tease");
         }
     }
-})
+}
